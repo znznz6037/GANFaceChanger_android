8a06f09f5a1da9debd7f0b9d4ab20d45e3b4e4b2
pc1 <a_rod19@naver.com>
2020-08-28 23:12:04 +0900

    Revert "fileUpload"
    This reverts commit 5e262e6c

--- PALGrad/app/src/main/java/com/example/pal_grad/fragment/FaceChange.kt
+++ PALGrad/app/src/main/java/com/example/pal_grad/fragment/FaceChange.kt
@@ -2,27 +2,36 @@ package com.example.pal_grad.fragment
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.pal_grad.MainActivity
 import com.example.pal_grad.R
 
 import kotlinx.android.synthetic.main.face_change_fragment.*
 import kotlinx.android.synthetic.main.face_change_fragment.view.*
+import java.io.IOException
+import java.lang.reflect.Method
+import java.security.Permission
+import java.util.jar.Manifest
 
 
 class FaceChange : Fragment() {
     private val OPEN_GALLERY = 1
     private val OPEN_CAMERA = 2
+
     private fun openGalleryForImage() {
-            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            val intent = Intent(Intent.ACTION_PICK)
             intent.type = "image/*"
-            startActivityForResult(Intent.createChooser(intent, "select picture "), OPEN_GALLERY)
+            startActivityForResult(intent, OPEN_GALLERY)
     }
     private fun captureCamera() {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
@@ -35,34 +44,10 @@ class FaceChange : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view:View = inflater!!.inflate(R.layout.face_change_fragment, container, false)
-        view.face_upload_button.setOnClickListener {
-            openGalleryForImage()
-        }
-        view.api_test.setOnClickListener {
-            (activity as MainActivity).apiTest()
-        }
 
         return view
     }
 
- /*   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK){
-            when(requestCode) {
-               OPEN_GALLERY -> {
-                    val uri = data?.data
-                    if (uri != null) {
-                        faceImageView.setImageURI(uri) // handle chosen image
-                        (activity as MainActivity).createImageData(uri)
-                    }
-                }
-                OPEN_CAMERA -> {
-                    faceImageView.setImageBitmap(data?.extras?.get("data") as Bitmap)
-                }
-            }
-        }
-    }*/
 
     companion object {
         fun create(): FaceChange {
