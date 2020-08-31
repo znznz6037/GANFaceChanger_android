29995e618fa4669c360c5997ebf8e53d13bf5737
pc1 <a_rod19@naver.com>
2020-08-31 17:23:21 +0900

    POST

--- PALGrad/app/src/main/java/com/example/pal_grad/api/StarGANAPI.kt
+++ PALGrad/app/src/main/java/com/example/pal_grad/api/StarGANAPI.kt
@@ -1,9 +1,11 @@
 package com.example.pal_grad.api
 
+
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface StarGANAPI {
+
     @GET("/result")
     fun getResult() : Call<StarGANResult>
 }
---
+++ PALGrad/app/src/main/java/com/example/pal_grad/api/response/Default.kt
@@ -0,0 +1,8 @@
+package com.example.pal_grad.api.response
+
+import com.google.gson.annotations.SerializedName
+
+data class Default (
+        @SerializedName("message")
+        var message:String?
+)
